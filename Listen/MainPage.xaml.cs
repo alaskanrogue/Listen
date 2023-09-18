2c947f8df16ee37c8000ff46acfd435fc32f0197
Marc George <mwg@thoughts.net>
2023-09-18 07:15:41 -0400

    Add project files.

---
+++ Listen/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace Listen
+{
+	using Microsoft.Maui;
+	using Microsoft.Maui.Controls;
+	using CommunityToolkit.Maui.Alerts;
+	using CommunityToolkit.Maui.Core;
+	using CommunityToolkit.Maui.Media;
+	using System.Globalization;
+	using System.Threading;
+
+	public partial class MainPage : ContentPage
+	{
+		internal static string activeLanguage = "en-US";
+		internal CancellationToken cancellationToken;
+		internal bool yesno;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnListenClicked(object sender, EventArgs e)
+		{
+			GetVocalResponse();
+		}
+
+		internal async void GetVocalResponse()
+		{
+			cancellationToken = new CancellationToken();
+
+			var RecognitionText = "";
+
+			ISpeechToText speechToText = SpeechToText.Default;
+
+			var recognitionResult = await speechToText.ListenAsync(
+												CultureInfo.GetCultureInfo(activeLanguage),
+												new Progress<string>(partialText =>
+												{
+													RecognitionText += partialText + " ";
+												}), cancellationToken);
+
+			if (recognitionResult.IsSuccessful)
+			{
+				RecognitionText = recognitionResult.Text;
+
+				if (RecognitionText.Contains("yes"))
+				{
+					yesno = true;
+				}
+				else
+				{
+					yesno = false;
+				}
+			}
+		}
+	}
+}
---
+++ Listen/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Listen
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
